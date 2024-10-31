e4f523f1376ab5892088e6f780449f4d2b1e7533
Lucian Hymer <lucian@gitcoin.co>
2024-10-31 12:17:53 -0600

    docs: updating README

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-export {
-  PassportScoreWidget,
-  PassportScoreWidgetProps,
-} from "./widgets/PassportScoreWidget";
-export { PassportWidgetProps, PassportWidgetTheme } from "./widgets/Widget";
+export { PassportScoreWidget } from "./widgets/PassportScoreWidget";
+export type { PassportScoreWidgetProps } from "./widgets/PassportScoreWidget";
+export type {
+  PassportWidgetProps,
+  PassportWidgetTheme,
+} from "./widgets/Widget";
