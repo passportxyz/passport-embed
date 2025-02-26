7d629f9fda24c202e082095179fa15d9ea01d78f
GHA Workflow <->
2025-02-26 12:46:13 +0000

    New build + update package version to v0.1.5

---
+++ dist/components/Body/PlatformVerification.d.ts
@@ -0,0 +1,6 @@
+import { Platform } from "../../hooks/useStampPages";
+export declare const PlatformVerification: ({ platform, onClose, generateSignatureCallback, }: {
+    platform: Platform;
+    onClose: () => void;
+    generateSignatureCallback: (message: string) => Promise<string | undefined>;
+}) => import("react/jsx-runtime").JSX.Element;
