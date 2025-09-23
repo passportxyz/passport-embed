1b0792b669e974243a077404bb720c17cc2ea09a
Lucian Hymer <lucian@gitcoin.co>
2025-09-23 17:14:07 -0600

    feat: progress on multi-error display

--- src/components/Body/PlatformHeader.tsx
+++ src/components/Body/PlatformHeader.tsx
@@ -1,8 +1,17 @@
 import styles from "./Body.module.css";
 import { Platform } from "../../hooks/stampTypes";
 import { usePlatformStatus } from "../../hooks/usePlatformStatus";
+import { TextButton } from "../TextButton";
 
-export const PlatformHeader = ({ platform }: { platform: Platform }) => {
+export const PlatformHeader = ({
+  platform,
+  showSeeDetails,
+  onSeeDetails,
+}: {
+  platform: Platform;
+  showSeeDetails: boolean;
+  onSeeDetails: () => void;
+}) => {
   const { pointsGained } = usePlatformStatus({ platform });
 
   return (
@@ -10,6 +19,7 @@ export const PlatformHeader = ({ platform }: { platform: Platform }) => {
       {platform.icon && <span className={styles.platformHeaderIcon}>{platform.icon}</span>}
       <span className={styles.platformHeaderName}>{platform.name}</span>
       {parseFloat(pointsGained) > 0 && <div className={styles.platformHeaderPoints}>{pointsGained}</div>}
+      {showSeeDetails && <TextButton onClick={onSeeDetails}>See Details ➜</TextButton>}
     </div>
   );
 };
--- src/components/Body/StampClaimResult.tsx
+++ src/components/Body/StampClaimResult.tsx
@@ -8,48 +8,106 @@ import { CredentialError } from "../../hooks/usePassportScore";
 import { AmbivalentHuman } from "../../assets/ambivalentHuman";
 import { TooltipIcon } from "../../assets/tooltipIcon";
 import { Tooltip } from "../Tooltip";
+import { useState } from "react";
+import { TextButton } from "../TextButton";
 
 type StampClaimSuccessProps = BackToStampsButtonProps & {
   platform: Platform;
   errors?: CredentialError[];
 };
 
+const ChevronRight = (props: any) => (
+  <TextButton {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 18L15 12L9 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  </TextButton>
+);
+
+const ChevronLeft = (props: any) => (
+  <TextButton {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15 18L9 12L15 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  </TextButton>
+);
+
 export const StampClaimResult = ({ platform, onBack, errors }: StampClaimSuccessProps) => {
   const { claimed } = usePlatformStatus({ platform });
+  const [seeErrors, setSeeErrors] = useState(false);
+  const [errorIndex, setErrorIndex] = useState(0);
 
   return (
     <>
-      <div className={`${styles.blurEffect} ${claimed ? "" : styles.altBlurColor}`} />
-      <PlatformHeader platform={platform} />
-      <div className={`${styles.textBlock} ${styles.tight}`}>
-        <div className={styles.iconWrapper}>{claimed ? <HappyHuman /> : <AmbivalentHuman />}</div>
-        <div className={`${styles.heading} ${styles.textCenter}`}>
-          {claimed ? "Congratulations!" : "Stamp Verification Unsuccessful"}
-        </div>
-        <div className={styles.textCenter}>
-          {claimed ? (
-            <>
-              You've verified credentials within
-              <br />
-              the {platform.name} Stamp
-            </>
-          ) : (
-            "Please try verifying another Stamp"
-          )}
-        </div>
-        {!claimed && (
-          <div className={`${styles.textCenter} ${styles.learnMore}`}>
-            {errors ? (
-              <Tooltip content={errors.map(error=>error.error).join()}>
-                <TooltipIcon />
-              </Tooltip>
-            ) : (
-              <TooltipIcon />
-            )}{" "}
-            Learn more
+      <PlatformHeader
+        platform={platform}
+        showSeeDetails={Boolean(errors?.length && !seeErrors)}
+        onSeeDetails={() => setSeeErrors(true)}
+      />
+      {seeErrors && errors?.length ? (
+        <div className={styles.errorSection}>
+          <div className={styles.errorSubHeader}>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M9.99984 6.66602V9.99935M9.99984 13.3327H10.0082M18.3332 9.99935C18.3332 14.6017 14.6022 18.3327 9.99984 18.3327C5.39746 18.3327 1.6665 14.6017 1.6665 9.99935C1.6665 5.39698 5.39746 1.66602 9.99984 1.66602C14.6022 1.66602 18.3332 5.39698 18.3332 9.99935Z"
+                stroke="rgb(var(--color-failure-c6dbf459))"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span>Error{errors.length > 1 ? "s" : ""}</span>
+            <span className={styles.errorCounterSpan}>{errors.length > 1 ? "Counter" : ""}</span>
+            {errors.length > 1 && (
+              <div>
+                <ChevronLeft
+                  className={styles.errorArrow}
+                  onClick={() => setErrorIndex((i) => Math.max(0, i - 1))}
+                  disabled={errorIndex === 0}
+                />
+                <ChevronRight
+                  className={styles.errorArrow}
+                  onClick={() => setErrorIndex((i) => Math.min(errors.length - 1, i + 1))}
+                  disabled={errorIndex === errors.length - 1}
+                />
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <>
+          <div className={`${styles.blurEffect} ${claimed ? "" : styles.altBlurColor}`} />
+          <div className={`${styles.textBlock} ${styles.tight}`}>
+            <div className={styles.iconWrapper}>{claimed ? <HappyHuman /> : <AmbivalentHuman />}</div>
+            <div className={`${styles.heading} ${styles.textCenter}`}>
+              {claimed ? "Congratulations!" : "Stamp Verification Unsuccessful"}
+            </div>
+            <div className={styles.textCenter}>
+              {claimed ? (
+                <>
+                  You've verified credentials within
+                  <br />
+                  the {platform.name} Stamp
+                </>
+              ) : (
+                "Please try verifying another Stamp"
+              )}
+            </div>
+            {!claimed && (
+              <div className={`${styles.textCenter} ${styles.learnMore}`}>
+                {errors ? (
+                  <Tooltip content={errors.map((error) => error.error).join()}>
+                    <TooltipIcon />
+                  </Tooltip>
+                ) : (
+                  <TooltipIcon />
+                )}{" "}
+                Learn more
+              </div>
+            )}
+          </div>
+        </>
+      )}
       <BackToStampsButton onBack={onBack} />
     </>
   );
